2f1b2b7747b94372adb22c513c4ebc68d83ddf9b
yuasalily <yuasalily1011@gmail.com>
2022-04-27 02:25:35 +0900

    henaserver

--- webbook/src/main.rs
+++ webbook/src/main.rs
@@ -5,6 +5,6 @@ mod client;
 
 fn main() {
     let tcp_server =  server::TcpServer{};
-    tcp_server.serve("172.17.0.2:80".to_string());
+    tcp_server.serve("172.17.0.2:80".to_string(), false);
     // client::tcp_client::request("127.0.0.1:80".to_string());
 }
--- webbook/src/server.rs
+++ webbook/src/server.rs
@@ -2,6 +2,7 @@ use std::net::{TcpListener, TcpStream};
 use std::io::{Read, Write};
 use std::fs::File;
 use std::fs;
+use chrono::Utc;
 
 pub mod cookie;
 
@@ -11,25 +12,51 @@ pub struct TcpServer{
 }
 
 impl TcpServer{
-    pub fn serve(&self, addr:String){
+    pub fn serve(&self, addr:String, save_recv_flg: bool){
+        println!("サーバを起動します");
         let listener = TcpListener::bind(addr).unwrap();
         let (mut stream, _) = listener.accept().unwrap();
-        let mut file = File::create("server_recv.txt").unwrap();
-        let mut buf = [0u8; 1024];
-        let r = stream.read(&mut buf).unwrap();
-        let _ = file.write_all(&buf[..r]);
         
-        let mut file = File::open("server_send.txt").unwrap();
-        let mut send_buf = Vec::new();
-        let _ = file.read_to_end(&mut send_buf);
-        println!("{:?}",send_buf);
-        let _ = stream.write(&send_buf);
+        // クライアントからのレクエストを取得
+        let mut request = [0u8; 1024];
+        let r = stream.read(&mut request).unwrap();
+        let request = request[..r].to_vec();
+        
+        if save_recv_flg {
+            println!("クライアントのリクエストを保存します");
+            let mut file = File::create("server_recv.txt").unwrap();
+            let _ = file.write_all(&request);
+        }
+        let mut response: String = "HTTP/1.1 200 OK\r\n".to_string(); 
+        let mut response_body: String = "<html><body><h1>It works!</h1></body></html>".to_string();
+
+        let mut response_head: String = chrono::Utc::now().format("Date: %a, %d %b %Y %H:%M:%S GMT\r\n").to_string();
+        response_head += "Host: HenaServer/0.1\r\n";
+        response_head += &format!("Content-Length: {}\r\n", response_body.as_bytes().len());
+        response_head += "Connection: Close\r\n";
+        response_head += "Content-Type: text/html\r\n";
+        response_head += "\r\n";
+        
+        response.push_str(&response_head);
+        response.push_str(&response_body);
+
+        
+        println!("{:?}", response);
+        let response = response.as_bytes();
+        // let response = self._get_apache_text();
+        
+        let _ = stream.write(&response);
         let _ = stream.shutdown(std::net::Shutdown::Both);
     }
 
 
-    pub fn sample(&self){
-        println!("server");
+    pub fn _get_apache_text(&self) -> Vec<u8> {
+        println!("apacheのレスポンスとのコピーを取得");
+        let mut file = File::open("server_send.txt").unwrap();
+        let mut send_buf = Vec::new();
+        let _ = file.read_to_end(&mut send_buf);
+        
         cookie::print_cookie();
+        return send_buf;
     }
 }
